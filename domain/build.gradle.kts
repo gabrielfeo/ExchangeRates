1c933e1922e3d87df64b90e2983103f9bf2febb5
Gabriel Feo <gabriel@gabrielfeo.com>
2019-05-05 02:25:34 -0300

    Wrote unit tests for domain classes
    JUnit 5 was added to the project, and JDK 8 was specified.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -12,10 +12,21 @@ repositories {
     jcenter()
 }
 
-kotlin.sourceSets["main"].kotlin.srcDirs("src/main")
-kotlin.sourceSets["test"].kotlin.srcDirs("src/test")
-
 dependencies {
     // Kotlin standard library
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    // Unit testing
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("src/main")
+kotlin.sourceSets["test"].kotlin.srcDirs("src/test")
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
---
+++ domain/src/test/com/gabrielfeo/exchangerates/domain/tests/CommonCurrencyUnits.kt
@@ -0,0 +1,7 @@
+package com.gabrielfeo.exchangerates.domain.tests
+
+import com.gabrielfeo.exchangerates.domain.currency.CurrencyUnit
+
+internal val usDollar = CurrencyUnit("United States Dollar", "US$", "USD", 840)
+internal val brazilianReal = CurrencyUnit("Brazilian Real", "R$", "BRL", 986)
+internal val euro = CurrencyUnit("Euro", "€", "EUR", 978)
