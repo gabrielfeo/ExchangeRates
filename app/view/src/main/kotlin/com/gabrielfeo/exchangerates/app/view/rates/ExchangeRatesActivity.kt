7db422899101c86246e119c939971dba9da1952b
Gabriel Feo <gabriel@gabrielfeo.com>
2019-05-07 12:13:27 -0300

    Fixed ExchangeRatesActivity launch
    ViewModel was being set before attach to Application.

--- app/view/src/main/kotlin/com/gabrielfeo/exchangerates/app/view/rates/ExchangeRatesActivity.kt
+++ app/view/src/main/kotlin/com/gabrielfeo/exchangerates/app/view/rates/ExchangeRatesActivity.kt
@@ -12,11 +12,12 @@ import com.gabrielfeo.exchangerates.app.view.databinding.ExchangeRatesActivityBi
 
 class ExchangeRatesActivity : AppCompatActivity() {
 
-    private val viewModel = ViewModelProviders.of(this).get<ExchangeRatesViewModel>()
+    private lateinit var viewModel: ExchangeRatesViewModel
     private lateinit var binding: ExchangeRatesActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get<ExchangeRatesViewModel>()
         binding = DataBindingUtil.setContentView(this, R.layout.exchange_rates_activity)
     }
 
